12af570dddc69089c15e3ad55ae98657b0957aa2
suvankar <suvankar.17@gmail.com>
2014-03-22 18:04:45 +0530

    Added functionality to create a task

--- todo/app/controllers/tasks_controller.rb
+++ todo/app/controllers/tasks_controller.rb
@@ -2,4 +2,15 @@ class TasksController < ApplicationController
   def index
   	@tasks = Task.all
   end
+
+  def create
+  	Task.create(task_params)
+  	redirect_to :back
+  end
+
+  private
+	  def task_params
+	  		params.require(:task).permit(:task)
+	  end
+
 end
--- todo/spec/requests/todos_spec.rb
+++ todo/spec/requests/todos_spec.rb
@@ -7,5 +7,13 @@ describe "Todos" do
       visit tasks_path
       page.should have_content 'make coffee'
     end
+
+    it "create a new task" do
+    	visit tasks_path
+    	fill_in :Task, :with => 'do some exercise'
+    	click_button 'Create Task'
+    	current_path.should == tasks_path
+    	page.should have_content 'do some exercise'
+    end
   end
 end
