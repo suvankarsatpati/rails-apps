c95525dfd1e989f693451072a3df152a17acdcad
suvankar <suvankar.17@gmail.com>
2014-03-22 17:14:59 +0530

    Created bare bone 'todo' app, bundeled with nessasary gems

---
+++ todo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todo::Application.config.secret_key_base = '97b0a156832b5817cbccc2daf2b67c19cf9db6ad5ebb64ed5545e951f2207739658e63b9cb5b51612b7ed524bd6807ad422af8d2c2b1aed44aafd0e46c33aadc'
