0513a02e666f09bfdadf9688b57a93b2649f28ef
suvankar <suvankar.17@gmail.com>
2014-03-29 08:57:36 +0530

    Blank Canvas

---
+++ explore-rack/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExploreRack::Application.config.secret_key_base = '6024b7b22de8bf99c38fb6782fea44022c8b41ac34a96c3da411fb03d5a6fab9ad0571de9b55ae220563147634fd79117a6ad9391a26e831483b56eb0a636971'
---
+++ explore-rack/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ExploreRack::Application.config.session_store :cookie_store, key: '_explore-rack_session'
