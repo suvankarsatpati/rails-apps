79e75ee50deb109c6cd90b96e9fe08e9b1ea1e7b
suvankar <suvankar.17@gmail.com>
2014-03-31 00:24:36 +0530

    Implemented a Product Catalog app with basic CRUD operation

--- explore-rack/config/routes.rb
+++ explore-rack/config/routes.rb
@@ -1,4 +1,7 @@
 ExploreRack::Application.routes.draw do
+  get "products/index"
+  root "products#index"
+  resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
